4026504894712680b7d624eb2bb6cb495d9ae29e
JY0R0 <boraljyotishko@gmail.com>
2026-05-01 00:05:53 +0530

    Port was hardcoded before and thus deployment could not read

--- server/server.js
+++ server/server.js
@@ -769,6 +769,7 @@ app.delete("/api/expenses/:expenseId", authenticateToken, (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
